fb7970ea3d06a2093841ac3026f09fd0ff2a8952
Leonardo <leonardo.fc2@outlook.com>
2019-08-21 09:25:39 -0300

    remove unused files

--- biscoito-da-sorte/pages/Frase.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Frase(){
-    return (
-        <div>
-            <h1>Pagina Frase</h1>
-        </div>
-    )
-}
--- biscoito-da-sorte/pages/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Main(){
-    return (
-        <div>
-            <h1>Pagina Main</h1>
-        </div>
-    )
-}
