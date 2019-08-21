09530f878187aa5dfe84f1a4cea5095db1a093f0
Leonardo <leonardo.fc2@outlook.com>
2019-08-21 09:17:45 -0300

    init app

---
+++ biscoito-da-sorte/pages/Frase.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Frase(){
+    return (
+        <div>
+            <h1>Pagina Frase</h1>
+        </div>
+    )
+}
--- biscoito-da-sorte/src/App.js
+++ biscoito-da-sorte/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-
-
+import Routes from './routes';
 function App() {
   return (
-    <div>
-      <h1>
-        oi
-      </h1>
-    </div>
+    <Routes/>
   );
 }
 
